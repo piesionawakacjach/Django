dfb0cc4cd3674dda3b1b10c4cb70d8beec677466
piesionawakacjach <piesionawakacjach@gmail.com>
2026-06-17 14:56:36 +0200

    Pierwsza wizualizacja - czwarty dzień 2

--- config/urls.py
+++ config/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from devboard import views
 
 #from devboard import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index, name="index")
+    path("", views.ProjectListView.as_view(), name="lista-project"),
+    path("accounts/", include("django.contrib.auth.urls"))
 ]
 
--- devboard/views.py
+++ devboard/views.py
@@ -1,9 +1,26 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from devboard.models import Project
 
+from django.db.models import Count
 # def index(request):
 #     return HttpResponse("<h1>Devboard - etap 1: scaffold!</h1>")
 
-def index(request):
-    return render(request, "index.html")
+# def index(request):
+#     return render(request, "index.html")
+#
 
+
+class ProjectListView(ListView, LoginRequiredMixin):
+    model = Project
+    template_name = "devboard/project_list.html"
+    context_object_name = "projects"
+
+    def get_queryset(self):
+        return (
+            Project.objects.filter(owner=self.request.user)
+            .annotate(task_count=Count("tasks"))
+            .order_by("-created_at")
+        )
